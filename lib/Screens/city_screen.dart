38456508157c81a0dc4db2b86487d510c86598bb
Ziga Larissa <l.bizimungu@alustudent.com>
2024-03-19 17:06:47 +0200

    HomeScreen and CityScreen updated

---
+++ lib/Screens/city_screen.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CityScreen extends StatelessWidget {
+  const CityScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 70),
+            const CircleAvatar(
+              radius: 120, // Image radius
+              backgroundImage: AssetImage('../images/city.jpg'),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Choose Your City',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF4F0B79),
+              ),
+            ),
+            const SizedBox(height: 50),
+            Container(
+              height: 44.0,
+              width: 300.0,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 8, 2, 173),
+                    Color.fromARGB(255, 172, 7, 238)
+                  ],
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(22)),
+              ),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent),
+                child: const Text(
+                  'Kigali',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 150),
+            const Text(
+              'Coming Soon...',
+              style: TextStyle(
+                fontSize: 18,
+                color: Color.fromARGB(255, 3, 3, 3),
+              ),
+            ),
+            Column(
+              children: [
+                Opacity(
+                  opacity: 0.6,
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    width: 400,
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 30, // Image radius
+                              backgroundImage:
+                                  AssetImage('../images/london.jpg'),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'London',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 30, // Image radius
+                              backgroundImage:
+                                  AssetImage('../images/newyork.jpg'),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'New York',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 30, // Image radius
+                              backgroundImage:
+                                  AssetImage('../images/istanbul.jpg'),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'Istanbul',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 30, // Image radius
+                              backgroundImage:
+                                  AssetImage('../images/manchester.jpg'),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'Manchester',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Container(
+              height: 400,
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              // color: Colors.red,
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CircleAvatar(
+                        radius: 30, // Image radius
+                        backgroundImage: AssetImage('../images/Ellipse1.jpg'),
+                      ),
+                      CircleAvatar(
+                        radius: 20, // Image radius
+                        backgroundImage: AssetImage('../images/Ellipse2.jpg'),
+                      ),
+                      CircleAvatar(
+                        radius: 40, // Image radius
+                        backgroundImage: AssetImage('../images/Ellipse3.jpg'),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                        radius: 40, // Image radius
+                        backgroundImage: AssetImage('../images/Ellipse4.jpg'),
+                      ),
+                      CircleAvatar(
+                        radius: 90, // Image radius
+                        backgroundImage: AssetImage('../images/Ellipse5.jpg'),
+                      ),
+                      CircleAvatar(
+                        radius: 34, // Image radius
+                        backgroundImage: AssetImage('../images/Ellipse6.jpg'),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CircleAvatar(
+                        radius: 50, // Image radius
+                        backgroundImage: AssetImage('../images/Ellipse7.jpg'),
+                      ),
+                      CircleAvatar(
+                        radius: 28, // Image radius
+                        backgroundImage: AssetImage('../images/Ellipse8.jpg'),
+                      ),
+                      CircleAvatar(
+                        radius: 68, // Image radius
+                        backgroundImage: AssetImage('../images/Ellipse9.jpg'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Welcome to\n GenaAi!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF4F0B79),
+                height: 1.2,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Discover the right price\n for your house, invest wisely.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                color: Color.fromARGB(255, 3, 3, 3),
+              ),
+            ),
+            const SizedBox(height: 50),
+            Container(
+              height: 44.0,
+              width: 400.0,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 8, 2, 173),
+                    Color.fromARGB(255, 172, 7, 238)
+                  ],
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(22)),
+              ),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent),
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
